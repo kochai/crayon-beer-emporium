7a0ac09e0f00e6e86e71021a49d68cdda3453c43
Marko Kovacevic <mr.markokovacevic@gmail.com>
2024-09-30 14:53:43 +0200

    Remove container and header element from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import LandingPage from './components/LandingPage';
+import LandingPage from './components/pages/LandingPage';
 
 function App() {
     return (
         <Router>
-            <div className="App">
-                <header>
-                    <h1 className="text-3xl font-bold underline">Craft Beer Emporium</h1>
-                </header>
-                <Routes>
-                    <Route path="/" element={<LandingPage/>}/>
-                </Routes>
-            </div>
+            <Routes>
+                <Route path="/" element={<LandingPage/>}/>
+            </Routes>
         </Router>
     );
 }
