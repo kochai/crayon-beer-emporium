23fa984072922b311b093547c44cae47d4569653
Marko Kovacevic <mr.markokovacevic@gmail.com>
2024-09-30 12:52:21 +0200

    Add LandingPage.tsx

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,9 @@
+import {FC} from "react";
+
+const LandingPage: FC = () => {
+    return (
+        <h1 className="text-3xl font-bold underline">Landing Page</h1>
+    )
+}
+
+export default LandingPage;
