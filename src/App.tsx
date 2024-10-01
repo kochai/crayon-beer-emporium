e5196ed11fa4c5f7662cebe17160ca718d5377e3
Marko Kovacevic <mr.markokovacevic@gmail.com>
2024-10-01 13:45:49 +0200

    Add BeerDetailsPage to App, Add Lazy Loading to component routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import LandingPage from './components/pages/LandingPage';
+import LoadingSpinner from './shared/LoadingSpinner';
+
+const BeerCatalogPage = lazy(() => import('./features/beerCatalog/components/BeerCatalogPage'))
+const BeerDetailsPage = lazy(() => import('./features/beerDetails/components/BeerDetailsPage'))
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<LandingPage/>}/>
+                <Route exact path="/" element={
+                    <Suspense fallback={<LoadingSpinner/>}>
+                        <BeerCatalogPage/>
+                    </Suspense>
+                }/>
+                <Route path="/beer/:id" element={
+                    <Suspense fallback={<LoadingSpinner/>}>
+                        <BeerDetailsPage/>
+                    </Suspense>
+                }/>
             </Routes>
         </Router>
     );
