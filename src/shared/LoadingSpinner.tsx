6456daf1e9701184fe788d9aad62a63db8695e8b
Marko Kovacevic <mr.markokovacevic@gmail.com>
2024-10-02 10:51:26 +0200

    Add variants to LoadingSpinner shared component

--- src/shared/LoadingSpinner.tsx
+++ src/shared/LoadingSpinner.tsx
@@ -1,7 +1,16 @@
-import {FC} from 'React';
+import {FC} from 'react';
+import clsx from 'clsx';
 
-const LoadingSpinner: FC = () => (
-    <div className="flex justify-center items-center h-screen">
+type LoadingSpinnerProps = {
+    variant?: 'inline' | 'full-screen'
+}
+
+const LoadingSpinner: FC<LoadingSpinnerProps> = ({variant = 'full-screen'}) => (
+    <div className={clsx([
+        "flex justify-center items-center",
+        variant === 'inline' && "h-full w-full absolute top-0",
+        variant === 'full-screen' && "h-screen"
+    ])}>
         <div
             className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"
             aria-label="Loading"
