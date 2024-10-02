cb5dc0ff6475ebbf3b6a247277425be021d3114a
Marko Kovacevic <mr.markokovacevic@gmail.com>
2024-10-02 11:31:37 +0200

    Fix BeerCatalogPage test case

--- src/features/beerCatalog/components/BeerCatalogPage.test.tsx
+++ src/features/beerCatalog/components/BeerCatalogPage.test.tsx
@@ -1,10 +1,15 @@
 import {render, screen} from '@testing-library/react'
 import {describe, it, expect} from 'vitest'
+import {MemoryRouter} from 'react-router-dom';
 import BeerCatalogPage from './BeerCatalogPage'
 
 describe('BeerCatalogPage', () => {
     it('renders loading indicator', () => {
-        render(<BeerCatalogPage/>);
+        render(
+            <MemoryRouter>
+                <BeerCatalogPage/>
+            </MemoryRouter>
+        );
         expect(screen.getByLabelText('Loading')).toBeInTheDocument();
     })
 })
